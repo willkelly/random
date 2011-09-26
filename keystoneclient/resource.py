f476caf30b4646b94cdf430a5e74bf45e3ed2ac6
Will Kelly <the.william.kelly@gmail.com>
2011-09-25 20:59:34 -0500

    Switched to a more normal style of python

---
+++ keystoneclient/resource.py
@@ -0,0 +1,38 @@
+from urllib2 import Request, urlopen, HTTPError
+import json
+
+class Resource(object):
+    def __init__(self, url, token, oname):
+        self.url = url
+        self.token = token
+        self.oname = oname
+    def _req(self, method="GET", data=None, xheaders=[], inst=None):
+        url = self.url
+        if inst:
+            url += "/%d" % (inst)
+        req = Request(url=url)
+        req.get_method = lambda: method
+        req.add_header("X-Auth-Token", self.token)
+        req.add_header("Content-type","application/json")
+        req.add_header("Accept","application/json")
+        if isinstance(data, str):
+            req.data = data
+        elif data:
+            req.data = json.dumps(data)
+        r = urlopen(req).read()
+        if r == "":
+            return { str(self.oname): None }
+        else:
+            return json.loads(r)
+    def _data(self, **kwargs):
+        if not kwargs.has_key(self.oname):
+            kwargs = { str(self.oname): kwargs }
+        return kwargs
+    def get(self, inst=None, **kwargs):
+        return self._req(method="GET", inst=inst)
+    def create(self, **kwargs):
+        return self._req(method="POST", inst=None, data=self._data(**kwargs))
+    def update(self, inst=None, **kwargs):
+        return self._req(method="PUT", inst=inst, data=self._data(**kwargs))
+    def delete(self, inst=None, **kwargs):
+        return self._req(method="DELETE", inst=inst, data=None)
