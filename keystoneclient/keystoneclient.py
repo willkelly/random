536dda25830e5f1e1c834d1050fab825c8aa27d0
Will Kelly <the.william.kelly@gmail.com>
2011-11-02 18:36:20 -0500

    Update for version detection in auth

--- keystoneclient/keystoneclient.py
+++ keystoneclient/keystoneclient.py
@@ -1,82 +1,72 @@
 from urllib2 import Request, urlopen, HTTPError
 import json
 
+
 class KeystoneClient(object):
-    '''A simple, stupid keystone client.
-TODO: Auth (requires pregenerated token at this time)
-Example Usage:
->>> from keystoneclient import KeystoneAdminClient
->>> ks = KeystoneAdminClient("http://127.0.0.1:5001/v2.0", "999888777666")
->>> ks.tenants.get()
-{u'tenants': {u'values': [{u'enabled': 1, u'id': u'demo', u'description': None},
-{u'enabled': 1, u'id': u'admin', u'description': u'test'}], u'links': []}}
->>> ks.tenants.get('demo')
-{u'tenant': {u'enabled': 1, u'id': u'demo', u'description': None}}
-'''
-    def __init__(self, url="http://localhost:5001/v2.0", token=None,
-                 service_url=None, oname=None, root=None, 
+    '''A simple, stupid keystone client.'''
+    def __init__(self, admin_url="http://localhost:5001/v2.0", token=None,
+                 service_url=None, oname=None, root=None,
                  user=None, password=None):
-        '''Create a KeystoneClient:
-Arguments:
-url - the public url to a keystone resource, eg http://keystone.domain.com:5001/v2.0
-token - a valid token for keystone use
-oname - the singular name of the resource you are pointed to (usually can be omitted)
-root - the root object (first instance) of the KeystoneClient, used to share the token
-'''
-        self.url = url
+        self.admin_url = admin_url
         self.service_url = service_url
         self.user = user
         self.password = password
         self.root = root or self
+        self.version = "unknown"
         if token:
             self.root.token = token
         else:
             self.authenticate()
-        # We'll use oname if provided or guess it is resource minus 's' (tenant / tenants)
-        self.oname = oname or url.rsplit("/")[-1][0:-1]
+        # We'll use oname if provided or resource minus 's' (tenant / tenants)
+        self.oname = oname or admin_url.rsplit("/")[-1][0:-1]
 
     def _req(self, method="GET", data=None, xheaders=[], inst=None):
         # make a keystone request
-        url = self.url
+        url = self.admin_url
         if inst:
             url += "/%s" % (inst)
         req = Request(url=url)
         req.data = data
         req.get_method = lambda: method
         req.add_header("X-Auth-Token", self.root.token)
-        req.add_header("Content-type","application/json")
-        req.add_header("Accept","application/json")
+        req.add_header("Content-type", "application/json")
+        req.add_header("Accept", "application/json")
         r = urlopen(req).read()
         if r == "":
-            return { str(self.oname): None }
+            return {str(self.oname): None}
         else:
             return json.loads(r)
 
     def authenticate(self):
         """Authenticate against service api"""
-        req = Request(url="%s/tokens" %self.service_url)
+        req = Request(url="%s/tokens" % self.root.service_url)
         req.add_header("Content-Type", "application/json")
-        req.data = json.dumps({ "passwordCredentials": { 
-                    "username": self.root.user,
-                    "password": self.root.password }
-                                })
         try:
-            r = json.loads(urlopen(req).read())
+            try:
+                req.data = json.dumps({"passwordCredentials": {
+                            "username": self.root.user,
+                            "password": self.root.password}})
+                r = json.loads(urlopen(req).read())
+                self.root.version = "2.0a"
+            except HTTPError:
+                req.data = "{'auth': %s}" % req.data
+                r = json.loads(urlopen(req).read())
+                self.root.version = "2.0b"
             self.root.token = r['auth']['token']['id']
         except KeyError:
             raise ValueError("Failed to authenticate to keystone: %s" % r)
-        
+
     def _data(self, **kwargs):
-        # structure data in proper format, munge _ variables in kwargs to allow for use of reserved words
-        if not kwargs.has_key(self.oname):
-            kwargs = { str(self.oname): kwargs }
-        for k in [ k for k in kwargs[self.oname].keys() if k.find("_") == 0]:
+        # structure data, munge _ variables in kwargs to remove _
+        if not self.oname in args:
+            kwargs = {str(self.oname): kwargs}
+        for k in [k for k in kwargs[self.oname].keys() if k.find("_") == 0]:
             kwargs[self.oname][k[1:]] = kwargs[self.oname][k]
             del kwargs[self.oname][k]
         return json.dumps(kwargs)
 
     def get(self, inst=None, **kwargs):
-        """gets data for the requested resource.  if inst is none, gets all instances"""
+        """gets data for the requested resource"""
         return self._req(method="GET", inst=inst)
 
     def create(self, **kwargs):
@@ -95,6 +85,7 @@ root - the root object (first instance) of the KeystoneClient, used to share the
         try:
             return object.__getattribute__(self, name)
         except AttributeError:
-            self.__dict__[name] = KeystoneClient("/".join([self.url, name]), 
+            self.__dict__[name] = KeystoneClient("/".join([self.admin_url,
+                                                           name]),
                                                  root=self.root)
         return object.__getattribute__(self, name)
