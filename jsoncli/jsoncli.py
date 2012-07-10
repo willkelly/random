ac16c73971c60adf462ca98b7179d4d2f0787c07
wilk <william.kelly@rackspace.com>
2012-07-10 13:45:35 -0500

    pep8

--- jsoncli/jsoncli.py
+++ jsoncli/jsoncli.py
@@ -5,12 +5,14 @@ import json
 import sys
 from copy import copy
 
+
 def SubstituteType(s, delim='='):
     try:
-        a, b = s.split("=",1)
-        return [a,json.loads(b)]
+        a, b = s.split("=", 1)
+        return [a, json.loads(b)]
     except:
         raise TypeError("No %s to split on" % delim)
+
 
 def JsonFileType(filename):
     try:
@@ -18,6 +20,7 @@ def JsonFileType(filename):
             return json.load(f)
     except:
         raise TypeError("Not a json file")
+
 
 def nested_sub(key, data, replace):
     if key == "/" or key == "":
@@ -29,7 +32,7 @@ def nested_sub(key, data, replace):
     while(len(indexes) > 0 and indexes[0] != ""):
         try:
             root = root["%s" % i]
-        except (TypeError, ValueError,KeyError):
+        except (TypeError, ValueError, KeyError):
             i = int(i)
             root = root[i]
         i, indexes = indexes[0], indexes[1:]
@@ -38,6 +41,7 @@ def nested_sub(key, data, replace):
     except TypeError:
         root[int(i)] = replace
     return r
+
 
 def nested_del(key, data):
     if key == "/" or key == "":
@@ -49,7 +53,7 @@ def nested_del(key, data):
     while(len(indexes) > 0 and indexes[0] != ""):
         try:
             root = root["%s" % i]
-        except (TypeError, ValueError,KeyError):
+        except (TypeError, ValueError, KeyError):
             i = int(i)
             root = root[i]
         i, indexes = indexes[0], indexes[1:]
@@ -59,10 +63,12 @@ def nested_del(key, data):
         del root[int(i)]
     return r
 
+
 class OpType(object):
     def __init__(self, op, conversion=lambda x: x):
         self.op = op
         self.conversion = conversion
+
     def __call__(self, d):
         r = [self.op]
         nd = self.conversion(d)
@@ -72,24 +78,28 @@ class OpType(object):
             r.append(nd)
         return r
 
+
 def substitute(data, root, sub):
     return nested_sub(root, data, sub)
+
 
 def delete(root, data):
     return nested_del(data, root)
 
+
 def main():
     parser = argparse.ArgumentParser(description="Parse and manipulate json, "
-                                     "specify one or more substitute and delete")
-    parser.add_argument('--substitute', '-s', action='append', 
+                                     "specify one or more substitute "
+                                     "and delete")
+    parser.add_argument('--substitute', '-s', action='append',
                         type=OpType(substitute, SubstituteType),
                         help='/path/to/json/root={"valid": "json"}',
                         dest="ops")
     parser.add_argument('--delete', '-d',
-                        action='append', 
+                        action='append',
                         help='/path/to/json/root',
                         dest="ops", type=OpType(delete))
-    parser.add_argument('file', action='store', 
+    parser.add_argument('file', action='store',
                         metavar='file',
                         type=JsonFileType)
     args = parser.parse_args()
@@ -103,11 +113,9 @@ def main():
     for op in args.ops:
         f, fargs = op[0], [r]
         fargs.extend(op[1:])
-        r = f(*fargs) 
+        r = f(*fargs)
     else:
         print json.dumps(r)
-
-
 
 if __name__ == '__main__':
     main()
