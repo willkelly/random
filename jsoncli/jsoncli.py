b2ed290e81f8d9e925590ca604ae0547610757e8
wilk <william.kelly@rackspace.com>
2012-07-10 23:32:21 -0500

    Add quiet and bail flags

--- jsoncli/jsoncli.py
+++ jsoncli/jsoncli.py
@@ -7,12 +7,13 @@ from functools import partial
 from copy import copy
 import collections
 
+
 def SubstituteType(s, delim='='):
     try:
-        a, b = s.split("=", 1)
+        a, b = s.split(delim, 1)
         return [a, json.loads(b)]
     except:
-        raise TypeError("No %s to split on" % delim)
+        raise TypeError("Not in key=valid_json format")
 
 
 def JsonFileType(filename):
@@ -23,7 +24,7 @@ def JsonFileType(filename):
         raise TypeError("Not a json file")
 
 
-def nested_op(key, data, f=lambda x,i: x[i], ret=None):
+def nested_op(key, data, f=lambda x, i: x[i], ret=None):
     if key == '/' or key == '':
         return ret
     indexes = key.strip("/").split("/")
@@ -40,10 +41,12 @@ def nested_op(key, data, f=lambda x,i: x[i], ret=None):
     f(root, i)
     return r
 
+
 def nested_sub(key, data, replace):
     def replaces(root, i):
         root[i] = replace
     return nested_op(key, data, f=replaces, ret=data)
+
 
 def nested_del(key, data):
     def delete(root, i):
@@ -86,23 +89,32 @@ def main():
                         action='append',
                         help='/path/to/json/root',
                         dest="ops", type=OpType(delete))
+    parser.add_argument('--bail', '-b',
+                        action='store_true',
+                        default=False)
+    parser.add_argument('--quiet', '-q',
+                        action='store_true',
+                        default=False)
     parser.add_argument('file', action='store',
                         metavar='file',
                         type=JsonFileType)
     args = parser.parse_args()
-    if not args.ops:
-        parser.print_help()
-        sys.exit(1)
+
     if not args.file:
         args.file = JsonFileType("/dev/stdin")
 
     r = args.file
     for op in args.ops:
-        f, fargs = op[0], [r]
-        fargs.extend(op[1:])
-        r = f(*fargs)
-    else:
-        print json.dumps(r)
+        try:
+            f, fargs = op[0], [r]
+            fargs.extend(op[1:])
+            r = f(*fargs)
+        except (KeyError, IndexError):
+            if not args.quiet:
+                sys.stderr.write("Failed to apply %s\n" % op)
+            if args.bail:
+                sys.exit(1)
+    print json.dumps(r)
 
 if __name__ == '__main__':
     main()
